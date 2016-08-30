2905a23e34969a1e334836d222458b607a3bc155
Saifullah Saifi <saifi@csdamu.com>
2016-08-31 01:16:35 +0530

    Create prog1.cpp

---
+++ prog1.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+
+/* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+int t,no,m,sw,sd;
+    scanf("%d", &t);
+    for(int i=0; i<t; i++)
+    {
+        scanf("%d %d %d", &no, &m, &sw);
+         sd=(sw+m-1)% no;
+
+        if(sd==0)
+             printf("%d\n",no);
+        else
+            printf("%d\n",sd);
+    }
+
+    return 0;
+}
